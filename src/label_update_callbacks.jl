af4338f30c3db85606c39b1d8a6b244954adf2b6
Ben Elkin <8xe8n-cm@online.de>
2025-11-28 11:34:55 +0100

    improvement on plot title/label updates

--- src/label_update_callbacks.jl
+++ src/label_update_callbacks.jl
@@ -8,13 +8,28 @@ function setup_label_update_callbacks(state, outputs)
     (; xlabel_text, ylabel_text, title_text, current_axis) = state.plot_handles
     plot_observable = outputs.plot
 
+        # on(plot_observable) do content
+        #     if content isa Figure
+        #         println("Plot generation completed.")
+        #         @show xlabel_text[]
+        #     end
+        # end
+
     # Update X-axis label when text field changes
     on(xlabel_text) do new_label
         ax = current_axis[]
         if !isnothing(ax) && new_label != ""
-            ax.xlabel = new_label
-            # Force plot refresh by notifying with same value
-            plot_observable[] = plot_observable[]
+            if ax.xlabel[] != new_label
+                ax.xlabel = new_label
+                # Force plot refresh by notifying with same value
+                #
+                # Doppelt gemoppelt hält besser:
+                # Otherwise on the first plot title/label update, 
+                # the first "Enter" is ignored
+                # Still, some "Enters" got ignored sometimes
+                plot_observable[] = plot_observable[]
+                plot_observable[] = plot_observable[]
+            end
         end
     end
     
@@ -22,8 +37,11 @@ function setup_label_update_callbacks(state, outputs)
     on(ylabel_text) do new_label
         ax = current_axis[]
         if !isnothing(ax) && new_label != ""
-            ax.ylabel = new_label
-            plot_observable[] = plot_observable[]
+            if ax.ylabel[] != new_label
+                ax.ylabel = new_label
+                plot_observable[] = plot_observable[]
+                plot_observable[] = plot_observable[]
+            end
         end
     end
     
@@ -31,8 +49,11 @@ function setup_label_update_callbacks(state, outputs)
     on(title_text) do new_title
         ax = current_axis[]
         if !isnothing(ax) && new_title != ""
-            ax.title = new_title
-            plot_observable[] = plot_observable[]
+            if ax.title[] != new_title
+                ax.title = new_title
+                plot_observable[] = plot_observable[]
+                plot_observable[] = plot_observable[]
+            end
         end
     end
 end
